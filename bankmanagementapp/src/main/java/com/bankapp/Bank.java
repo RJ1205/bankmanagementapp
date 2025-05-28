7638cf5fcfdcc95b30a0b313bbea1fafd3781c0e
st187149 <st187149@stud.uni-stuttgart.de>
2025-05-28 18:57:47 +0200

    Junit 5 und customer sowie bank angepasst
    paar klassen erweitert und Junit 5 implementiert

--- bankmanagementapp/src/main/java/com/bankapp/Bank.java
+++ bankmanagementapp/src/main/java/com/bankapp/Bank.java
@@ -1,5 +1,18 @@
 package com.bankapp;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Bank {
+    private Map<Integer, Customer> customers = new HashMap();
+    private Map<Integer, BankAccount> accounts = new HashMap<>();
+    private int accountId = 1;
+    private int customerId = 1;
+
+    public Customer createCustomer(String name, String address) {
+        Customer customer = new Customer(customerId++, name, address);
+        customers.put(customer.getId(), customer);
+        return customer;
+    }
     
 }
--- bankmanagementapp/src/main/java/com/bankapp/Customer.java
+++ bankmanagementapp/src/main/java/com/bankapp/Customer.java
@@ -1,5 +1,51 @@
 package com.bankapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
+    private int id;
+    private String name;
+    private String address;
+    private List<BankAccount> accounts = new ArrayList<>();    
+
+    public Customer(int id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<BankAccount> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<BankAccount> accounts) {
+        this.accounts = accounts;
+    }
     
+
 }
